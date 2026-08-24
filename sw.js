7a967772ae5f1a9c5476344d33430d026b5ff82d
wuyanmo1015 <wuyanmo1015@gmail.com>
2026-08-24 22:00:24 +0800

    deploy: sw.js

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE = 'erp-pwa-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/store.js',
+  './js/app.js',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png',
+  './apple-touch-icon.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  if (e.request.method !== 'GET' || url.origin !== self.location.origin) return;
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(e.request)
+        .then((resp) => {
+          const copy = resp.clone();
+          caches.open(CACHE).then((c) => c.put(e.request, copy));
+          return resp;
+        })
+        .catch(() => caches.match('./index.html'));
+    })
+  );
+});
